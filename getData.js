797334bab7133aeccf992c875d233e580a52aab4
Ajay Fewell <ajayfewell@gmail.com>
2017-10-19 20:29:05 -0400

    Changes to be committed: 	modified:   getData.js

--- getData.js
+++ getData.js
@@ -1,11 +1,26 @@
 const login = require("facebook-chat-api");
 
-user = "ajayfewell@gmail.com";
-pword = "0";
+var user = "ajayfewell@gmail.com";
+var psword = "";
 // Need secure way to get username+password and login
 
-login({email: user, password: pword}, (err, api) => {
+login({email: user, password: psword}, (err, api) => {
 	if (err) console.log(err);
 	
 	
-})
+	api.getThreadList(0, 20, "inbox", (err, arr) => {
+		for (var index in arr) { 
+			var friend = arr[index];
+			var friendName = "";
+			var friendID = friend.participantIDs[0];
+			
+			api.getUserInfo(friendID, (err, obj) => {
+				friendName = obj.name;
+				for(var prop in obj) {
+					console.log(obj[prop].name);
+				}
+				
+			});
+ 		}
+	});
+});
